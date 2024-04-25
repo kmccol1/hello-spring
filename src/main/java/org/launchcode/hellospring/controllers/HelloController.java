a97fbbbeebac02524560d13a34fb8f7b59a553b4
Kyle McColgan <kdmbus21@gmail.com>
2024-04-25 01:43:43 -0500

    Upload chapter ten textbook exercise part one.

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -51,5 +51,71 @@ public class HelloController
                 "</html>";
     }
 
+    public static String createMessage(String name, String language)
+    {
+        String result = "";
+
+        if(language.equalsIgnoreCase("French"))
+        {
+            result = "Bonjour," + name + "!";
+        }
+        else if (language.equalsIgnoreCase("German"))
+        {
+            result = "Guten tag," + name + "!";
+        }
+        else if (language.equalsIgnoreCase("Spanish"))
+        {
+            result = "Hola," + name + "!";
+        }
+        else if (language.equalsIgnoreCase("Italian"))
+        {
+            result = "Ciao," + name + "!";
+        }
+        else if (language.equalsIgnoreCase("Portuguese"))
+        {
+            result = "OLÀ," + name + "!";
+        }
+        else if (language.equalsIgnoreCase("Hindi"))
+        {
+            result = "Namaste," + name + "!";
+        }
+        else if (language.equalsIgnoreCase("Persian (Farsi)"))
+        {
+            result = "Salaam," + name + "!";
+        }
+
+        return result;
+    }
+
+    @GetMapping("greeting")
+    public String helloLanguageForm()
+    {
+        return "<html>" +
+                "<body>" +
+                "<form action ='helloLang' method='post'>" +
+                "<input type='text' name='name'>" +
+                "<select name='language'>" +
+                "<option value='French'>French </option>"+
+                "<option value='German'>German </option>"+
+                "<option value='Spanish'>Spanish </option>"+
+                "<option value='Italian'>Italian </option>"+
+                "<option value='Portuguese'>Portuguese </option>"+
+                "<option value='Hindi'>Hindi </option>"+
+                "<option value='Persian (Farsi)'>Persian (Farsi) </option>"+
+                "<input type='submit' value='Greet me!'>" +
+                "</form" +
+                "</body>" +
+                "</html>";
+    }
+
+    @RequestMapping(value="helloLang", method=RequestMethod.POST)
+    @ResponseBody
+    public String helloLang(@RequestParam String name, @RequestParam String language)
+    {
+        if(name ==null)
+            name = "World";
+        return createMessage(name, language);
+    }
+
 
 }
