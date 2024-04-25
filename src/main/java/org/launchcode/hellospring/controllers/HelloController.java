ec67a769c2fcb1506916d29a7921ae69ad5e4f3c
Kyle McColgan <kdmbus21@gmail.com>
2024-04-25 00:14:07 -0500

    Upload chapter ten textbook example code part three.

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,6 +2,7 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -28,4 +29,13 @@ public class HelloController
     {
         return "Hello," + name + "!";
     }
+
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name)
+    {
+        return "Hello," + name + "!";
+    }
+
+
 }
