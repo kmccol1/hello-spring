8547c34af27808c9dfbda78a92e595c51e7ad36b
Kyle McColgan <kdmbus21@gmail.com>
2024-04-25 00:06:29 -0500

    Upload chapter ten textbook example code part two.

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,22 +2,30 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController
 {
-    @GetMapping("hello") //argument represents the URL pattern...
-    @ResponseBody //plain text response, no template
-    public String hello()
-    {
-        return "Hello, Spring!";
-    }
+//    @GetMapping("hello") //argument represents the URL pattern...
+//    @ResponseBody //plain text response, no template
+//    public String hello()
+//    {
+//        return "Hello, Spring!";
+//    }
 
     @GetMapping("goodbye") //argument represents the URL pattern...
     @ResponseBody //plain text response, no template
     public String goodbye()
     {
         return "Goodbye, Spring!";
+    }
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name)
+    {
+        return "Hello," + name + "!";
     }
 }
