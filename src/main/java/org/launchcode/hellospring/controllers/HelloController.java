e1d7e050478c49791f7c813d7d11f2161750566c
Kyle McColgan <kdmbus21@gmail.com>
2024-04-25 00:25:16 -0500

    Upload chapter ten textbook example code part four.

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,10 +1,7 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController
@@ -23,7 +20,8 @@ public class HelloController
         return "Goodbye, Spring!";
     }
 
-    @GetMapping("hello")
+    //@GetMapping("hello")
+    @RequestMapping(method={RequestMethod.GET, RequestMethod.POST}, value="hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name)
     {
@@ -35,6 +33,20 @@ public class HelloController
     public String helloWithPathParam(@PathVariable String name)
     {
         return "Hello," + name + "!";
+    }
+
+    @GetMapping("form")
+    @ResponseBody
+    public String helloForm()
+    {
+        return "<html>" +
+                "<body>" +
+                "<form action ='hello' method='post'>" +
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form" +
+                "</body>" +
+                "</html>";
     }
 
 
